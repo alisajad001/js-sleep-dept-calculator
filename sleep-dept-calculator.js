5b374fee8556c515ae1809559edbee9aed68611b
Ali Sajad Ahmadi <forwebonly0@gmail.com>
2023-06-05 16:27:30 +0430

    Get actual sleep hours function added

--- sleep-dept-calculator.js
+++ sleep-dept-calculator.js
@@ -1,27 +1,30 @@
 // 1. Get sleep hours
 const getSleepHours = day => {
-  day = day.toLowerCase()
-  switch(day) {
-    case 'saturday':
-      return 5;
-    break;
-    case 'sunday':
-      return 5;
-    break;
-    case 'monday':
-      return 3;
-    break;
-    case 'tuesday':
-      return 4;
-    break;
-    case 'wednesday':
-      return 5;
-    break;
-    case 'thursday':
-      return 7;
-    break;
-    case 'friday':
-      return 3;
-    break;
-  }
+    day = day.toLowerCase()
+    switch(day) {
+      case 'saturday':
+        return 5;
+      break;
+      case 'sunday':
+        return 5;
+      break;
+      case 'monday':
+        return 3;
+      break;
+      case 'tuesday':
+        return 4;
+      break;
+      case 'wednesday':
+        return 5;
+      break;
+      case 'thursday':
+        return 7;
+      break;
+      case 'friday':
+        return 3;
+      break;
+    }
 }
+
+// 2. Get total sleep hours
+const getActualSleepHours = () => getSleepHours('saturday') + getSleepHours('sunday') + getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday');
